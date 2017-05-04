9ab686638e774f728c023889c94043a236ce3674
nicktimebreak <shxnick@gmail.com>
2017-05-04 17:37:04 +0800

    folder made

--- xiumm2.py
+++ xiumm2.py
@@ -1,5 +1,5 @@
 from multiprocessing.dummy import Pool as ThreadPool
-from bs4 import BeautifulSoup
+import os
 import requests
 import re
 
@@ -20,24 +20,33 @@ def create_folder(url):
 def download_img(url):
 	hs = requests.get(url)
 	hs.encoding = 'utf-8'
-	pattern = '(?<=/data).*\.jpg'
-	match = re.findall(pattern, hs.text)
-	img = []
-	for m in match:
-		img.append('http://www.xiumm.org/data'+m)
-	for i in img:
-		print(i+'\n')
+	pattern_url = '(?<=/data).*\.jpg'
+	match_url = re.findall(pattern_url, hs.text)
+	pattern_name = '\d{14}\.jpg'
+	img_url = []
+	img_name = []
+	for m in match_url:
+		img_url.append('http://www.xiumm.org/data'+m)
+		img_name.append(re.findall(pattern_name, m)[0])
+	for i in range(0,len(img_url)):
+		print(img_url[i]+'\n'+img_name[i]+'\n')
 
 if __name__ == '__main__':
-    url = "http://www.xiumm.org/photos/UGirls-17207.html"
-    page_total = 1
+	url = "http://www.xiumm.org/photos/UGirls-17207.html"
+	page_total = 1
     # 生成图像页码页面，用于map函数进行多线程下载
-    url_list = generate_url_list(url, page_total)
+	url_list = generate_url_list(url, page_total)
     # 生成的文件夹名字
-    print('Folder: '+create_folder(url))
-    # 后面的页码
-    pool = ThreadPool(4)
-    pool.map(download_img, url_list)
-    pool.close()
-    pool.join() 
+	folder_name = create_folder(url)
+	folder_path = r'G:\\工作目录\\Desktop\\' + folder_name
+	print('Folder: '+ folder_path)
+	if os.path.isdir(folder_path):
+		pass
+	else:
+		os.makedirs(folder_path)
+	pool = ThreadPool(4)
+	r= pool.map(download_img, url_list)
+	print(type(r))
+	pool.close()
+	pool.join() 
 
